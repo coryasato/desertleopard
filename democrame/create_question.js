7bf401f9703942ac1971070e13137b48e3fe7a44
Rene Polo <renejpolo@me.com>
2014-12-10 20:00:35 -0800

    voting sistem working

--- democrame/create_question.js
+++ democrame/create_question.js
@@ -6,15 +6,53 @@ Template.createQuestion.helpers({
   }
 });
 
+Template.results.helpers({
+  calculation: function(rating, votes){
+    var calculation = (rating / votes) * 100;
+    return calculation;
+  }
+})
+
 Template.createQuestion.events({
   'submit .new-question': function (evt, template) {
-     // var text = event.target.text.value;
      evt.preventDefault(); 
-     var text = template.find('#theText').value;
-    Meteor.call('createQuestion', {'text':text}, function(err) {
+     var questionBody = {
+          'text': template.find('#theText').value
+        }
+    Meteor.call('createQuestion', questionBody, function(err) {
       if(err) { console.log(err); }
     }); 
-  } 
+  },
+  'click #delete-question': function (evt, template) {
+      evt.preventDefault();
+      var rule = this;
+      var id = evt.currentTarget.name;
+      Meteor.call('remove', id, function(err) {
+      console.log('Remove Clicked', id);
+      if(err) { console.log(err); 
+      }
+    }); 
+  },
+  'click #vote-yes': function(evt, template){
+    evt.preventDefault();
+    var rule = this;
+    var id = evt.currentTarget.name;
+    Meteor.call('voteYes', id, function(err) {
+      console.log('vote yes Clicked', id);
+      if(err) { console.log(err); 
+      }
+    });
+  } ,
+  'click #vote-no': function(evt, template){
+    evt.preventDefault();
+    var rule = this;
+    var id = evt.currentTarget.name;
+    Meteor.call('voteNo', id, function(err) {
+      console.log('vote yes Clicked', id);
+      if(err) { console.log(err); 
+      }
+    });
+  }  
 });
 
 }
